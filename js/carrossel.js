06e7e32c00e4121e28cef9cea9e2d59dc18043e5
Diogo Costa <diogo.oliveira.costa@tecnico.ulisboa.pt>
2024-05-17 15:11:24 +0100

    fix: corrected ring rotation and added pseudo-parametric surfaces

--- js/carrossel.js
+++ js/carrossel.js
@@ -21,10 +21,12 @@ var keys = {};
 var axis;
 
 // Ring radius
-    const ringRadius = 4;
+    const ringRadius = 6;
+    const ringLength = 2;
+    const ringHeight = 6;
 
 // Cylinder dimensions
-    const cylinderRadius = 3;
+    const cylinderRadius = 4;
     const cylinderHeight = 7;
     const cylinderColor = 0xff0099;
 
@@ -67,9 +69,9 @@ function createScene() {
     rings.push(createRing(0, 0, 0, innerRingInnerRadius, innerRingOuterRadius, innerRingColor));
     rings.push(createRing(0, 0, 0, middleRingInnerRadius, middleRingOuterRadius, middleRingColor));
     rings.push(createRing(0, 0, 0, outerRingInnerRadius, outerRingOuterRadius, outerRingColor));
-    scene.add(rings[0]);
-    scene.add(rings[1]);
-    scene.add(rings[2]);
+    cylinder.add(rings[0]);
+    cylinder.add(rings[1]);
+    cylinder.add(rings[2]);
 }
 
 //////////////////////
@@ -117,16 +119,59 @@ function createRing(x, y, z, innerRadius, outerRadius, ringColor) {
 
     var ring = new THREE.Object3D();
 
-    geometry = new THREE.RingGeometry(innerRadius, outerRadius);
+    const shape = new THREE.Shape();
+    shape.moveTo(-ringLength/2, -ringHeight/2);
+    shape.lineTo(ringLength/2, -ringHeight/2);
+    shape.lineTo(ringLength/2, ringHeight/2);
+    shape.lineTo(-ringLength/2, ringHeight/2);
+    shape.lineTo(-ringLength/2, -ringHeight/2);
+
+    const path = new THREE.Curve();
+    const pathRadius = (innerRadius+outerRadius)/2;
+    path.getPoint = function (t) {
+        var segment = (2*Math.PI) * t;
+        return new THREE.Vector3(pathRadius * Math.cos(segment), pathRadius * Math.sin(segment), 0);
+    };
+
+    const extrudeSettings = {
+        steps: 64,
+        bevelEnabled: false,
+        extrudePath: path
+    };
+    
+    geometry = new THREE.ExtrudeGeometry( shape, extrudeSettings );
     material = new THREE.MeshBasicMaterial({ color: ringColor, wireframe: false });
     mesh = new THREE.Mesh(geometry, material);
     mesh.rotateX(3*Math.PI/2);
     moveUpRing.push(true);
 
     ring.add(mesh);
+    createParametricSurfaces(ring, innerRadius, outerRadius);
 
     ring.position.set(x, y, z);
     return ring;
+}
+
+/*function parametricSurface(u, v) {
+    var x = Math.sin(u) * Math.cos(v);
+    var y = Math.sin(u) * Math.sin(v);
+    var z = Math.cos(u);
+    return new THREE.Vector3(x, y, z);
+}*/
+
+function createParametricSurfaces(obj, innerRadius, outerRadius) {
+    'use strict';
+
+    for (let angle=0; angle < 2*Math.PI; angle += Math.PI/4) {
+        geometry = new THREE.BoxGeometry(1,1,1);
+        material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: false });
+        mesh = new THREE.Mesh(geometry, material);
+
+        mesh.translateOnAxis(new THREE.Vector3(Math.sin(angle), 0, Math.cos(angle)), (innerRadius+outerRadius)/2);
+        mesh.translateY(1/2+ringLength/2);
+
+        obj.add(mesh);
+    }
 }
 
 //////////////////////
