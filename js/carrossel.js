a7659c2447af4eda12ffc68c413024b5446fdb0f
Pedro Lima <pedro.f.c.lima@tecnico.ulisboa.pt>
2024-05-20 18:10:07 +0100

    refactor: minor change

--- js/carrossel.js
+++ js/carrossel.js
@@ -349,7 +349,7 @@ function moveRing(num) {
 }
 
 function moveSurfaces() {
-    for (let i=0; i<parametricSurfaces.length; i++) {
+    for (i=0; i<parametricSurfaces.length; i++) {
         parametricSurfaces[i].rotateY(surfaceVelocity);
     }
 }
